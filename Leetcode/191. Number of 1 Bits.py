65d0506c5de7a430800e4a493028c98d1bebf810
fanlingdong <z5295470@ad.unsw.edu.au>
2023-08-11 16:02:12 +1000

    leetcode 191. Number of 1 Bits

---
+++ Leetcode/191. Number of 1 Bits.py
@@ -0,0 +1,76 @@
+"""
+
+Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+
+Example 1:
+
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+Example 2:
+
+Input: n = 00000000000000000000000010000000
+Output: 1
+Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+Example 3:
+
+Input: n = 11111111111111111111111111111101
+Output: 31
+Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+
+
+Constraints:
+
+The input must be a binary string of length 32.
+
+"""
+from collections import Counter
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        c = Counter(str(bin(n)))
+
+        return c['1']
+
+class Solution2:
+    def hammingWeight(self, n: int) -> int:
+        str_n = str(bin(n)).replace('0b', '')
+
+        res = 0
+        for i in str_n:
+            if i == '1':
+                res += 1
+        return res
+
+
+class Solution3:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1
+            if bit | 0 == 1:
+                res += 1
+        return res
+
+class Solution4:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += n % 2
+            n = n >> 1
+        return res
+
+class Solution5:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            n &= n - 1
+            res += 1
+        return res
