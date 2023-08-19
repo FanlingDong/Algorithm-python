8d84b4a90463dfdf59103529b750e5b807cad3ad
fanlingdong <z5295470@ad.unsw.edu.au>
2023-08-19 22:57:57 +1000

    Leetcode: 49. Group Anagrams

--- Leetcode/49. Group Anagrams.py
+++ Leetcode/49. Group Anagrams.py
@@ -35,3 +35,15 @@ class Solution:
             dict[word].append(s)
 
         return list(dict.values())
+
+
+class Solution2:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
