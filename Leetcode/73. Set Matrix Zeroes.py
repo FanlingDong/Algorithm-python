0404a38221393cb52ecef115a0a86b9430669699
fanlingdong <z5295470@ad.unsw.edu.au>
2023-08-22 23:58:05 +1000

    Leetcode: 73. Set Matrix Zeroes

---
+++ Leetcode/73. Set Matrix Zeroes.py
@@ -0,0 +1,63 @@
+"""
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+
+
+Example 1:
+1 1 1   1 0 1
+1 0 1   0 0 0
+1 1 1   1 0 1
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-231 <= matrix[i][j] <= 231 - 1
+"""
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        # 2 array
+        rows, cols = len(matrix), len(matrix[0])
+        rowZero = False
+        # determine which rows/cols need to be zero
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    # col
+                    matrix[0][c] = 0
+                    # row
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+        if rowZero == True:
+            for c in range(cols):
+                matrix[0][c] = 0
+
