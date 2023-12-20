2abe1afafd37e394e06475c7c661a6df8eb9c619
fanlingdong <z5295470@ad.unsw.edu.au>
2023-12-20 23:53:03 +1100

    二叉搜索树的最小绝对差

--- Leetcode/530. Minimum Absolute Difference in BST.py
+++ Leetcode/530. Minimum Absolute Difference in BST.py
@@ -23,5 +23,21 @@ Output: 1
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.result = float('inf')
+        self.pre = None
+
+    def traversal(self, cur):
+        if cur is None:
+            return
+        self.traversal(cur.left)
+
+        if self.pre is not None:
+            self.result = min(self.result, cur.val - self.pre.val)
+        self.pre = cur
+        self.traversal(cur.right)
+
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        pass
+        self.traversal(root)
+        return int(self.result)
+
